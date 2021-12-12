13e407739085f7c9397027e093dd1d453a9efb0c
Rithvik <56070766+rithvik78@users.noreply.github.com>
2021-12-12 14:25:58 +0530

    Update models.py

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -7,7 +7,7 @@ from .extensions import db
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(512))
-    short_url = db.Column(db.String(3), unique=True)
+    short_url = db.Column(db.String(7), unique=True)
     visits = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now)
 
